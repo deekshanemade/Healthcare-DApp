04f300b0c52b26742e86266ab0955c57b58df1f2
BhagyaPisal <bhgyashreepisal12@gmail.com>
2021-03-12 22:06:13 +0530

    simple ui

--- dapp_ui/src/App.js
+++ dapp_ui/src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    // sign-in-page
+    <div class="sign-in-page-wrapper">
+    <div class="sign-in-page-container">
+        <input type="text"/>
+        <input type="text"/>
+        <button>Login</button>
     </div>
+</div>
+// profile-page
+<div class="user-main-page-wrapper">
+    <div class="left-tab-list">
+        <p class="nav-tab">Profile</p>
+        <p class="nav-tab">Medical History</p>
+        <p class="nav-tab">Insurance Claim</p>
+        <p class="nav-tab">Notification</p>
+    </div>
+    <div class="main-page-container">
+        <div class="profile-wrapper">
+            <img src="../../assets/icons/profile-user.svg" class="profile-pic"/>
+            <p class="user-name">UserName</p>
+            <p class="uid">P123456789</p>
+        </div>
+    <div class="medical-history-wrapper">
+        
+    </div>
+</div>
+</div>
+</div>
   );
 }
 
