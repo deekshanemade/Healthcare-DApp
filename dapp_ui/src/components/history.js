cefbb62e507f9fc1dc1c9dea2be5252e69e4ecac
BhagyaPisal <bhgyashreepisal12@gmail.com>
2021-05-15 19:16:34 +0530

    change

--- dapp_ui/src/components/history.js
+++ dapp_ui/src/components/history.js
@@ -4,18 +4,27 @@ import doc from '../assets/doc-icon.svg'
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
-import medical_details from './medical_details';
+import NewObservation from './newObservation';
+import { newContextComponents } from "@drizzle/react-components";
+const { ContractData, ContractForm } = newContextComponents;
+
+
 
 let showHistory = true;
 
-function history() {    
+function history({ drizzle, drizzleState }) {    
     const medicalHistory=[{Reason:"a",Description:"a"},{Reason:"b",Description:"b"},{Reason:"c",Description:"c"}]
     const history = useHistory();
+    let sender = drizzle.web3.eth.accounts.givenProvider.selectedAddress;
+    drizzle.contracts.Healthcare.methods.showRecord(1).call().then(res=>{
+            console.log(res);
+        })
     const handleEvent = () => history.push('/medical-details');
     if(showHistory){
     return (
         <>
         <div className="history-wrapper">
+        {/* <ContractData drizzle={drizzle} drizzleState={drizzleState} contract="Healthcare" method="showRecord" methodArgs={[1, { from: sender }]} /> */}
                 {medicalHistory.map((value,index)=>{
                     return  <div key={index} className="history" onClick={handleEvent}>
                         <img src={doc} className="document-image" alt=""/>
@@ -28,6 +37,7 @@ function history() {
                 })}
             
         </div>
+                <NewObservation drizzle={drizzle} drizzleState={drizzleState}></NewObservation>
                 </>
     )
             }
